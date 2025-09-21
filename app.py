ae5ccbb17b89557d6217f9d4f8fbe4a8ac4eea58
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-09-21 14:15:43 +0530

    implement OAuth registration for Google authentication

--- app.py
+++ app.py
@@ -15,3 +15,21 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY")
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
+
+
+oauth = OAuth(app)
+oauth.register(
+    name="google",
+    client_id=GOOGLE_CLIENT_ID,
+    client_secret=GOOGLE_CLIENT_SECRET,
+    access_token_url="https://oauth2.googleapis.com/token",
+    access_token_params=None,
+    authorize_url="https://accounts.google.com/o/oauth2/v2/auth",
+    authorize_params=None,
+    api_base_url="https://www.googleapis.com/oauth2/v3/",
+    client_kwargs={"scope": "openid email profile"},
+)
+
+# run app
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
